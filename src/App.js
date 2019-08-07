9d40ac0c70a036da34b0aad2e1cfced7ebddd22a
Noel Abellera <noel.abellera@gmail.com>
2019-08-06 22:00:50 -0700

    add CompChoice function

--- src/App.js
+++ src/App.js
@@ -6,16 +6,27 @@ class App extends Component {
     super(props);
     this.state = {
       playerScore: 0,
-      compScore: 0
+      compScore: 0,
+      choices: ['rock', 'papper', 'scissors'],
+      compChoice: ''
     }
   }
+
+  getCompChoice = () => {
+    this.setState({
+      compChoice: "Noel"
+    })
+    console.log(this.state.compChoice)
+  }
+
+
   render() {
   return (
     <div className="App">
       <h1>Rock-Paper-Scissors</h1>
       <h2>player Score: {this.state.playerScore}</h2>
       <h2>computer Score: {this.state.compScore}</h2>
-
+      <button onClick={this.getCompChoice}>Check</button>
       
     </div>
   );
