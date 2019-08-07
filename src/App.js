1cff7e03b8e4e3e324e6829b99ad2728fe33e384
Noel Abellera <noel.abellera@gmail.com>
2019-08-06 21:42:03 -0700

    Add state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerScore: 0,
+      compScore: 0
+    }
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rock-Paper-Scissors</h1>
+      <h2>player Score: {this.state.playerScore}</h2>
+      <h2>computer Score: {this.state.compScore}</h2>
+
+      
     </div>
   );
+  }
 }
 
 export default App;
