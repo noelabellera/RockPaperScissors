a6b2e6368da91b0722b27ded30a7198f82549a91
Noel Abellera <noel.abellera@gmail.com>
2019-09-01 18:51:57 -0700

    Add rock paper scissor buttons

--- src/App.js
+++ src/App.js
@@ -8,15 +8,36 @@ class App extends Component {
       playerScore: 0,
       compScore: 0,
       choices: ['rock', 'papper', 'scissors'],
-      compChoice: ''
+      compChoice: '',
+      playerChoice: ''
     }
   }
 
   getCompChoice = () => {
     this.setState({
-      compChoice: "Noel"
+      compScore: this.state.compScore + 1
     })
     console.log(this.state.compChoice)
+  }
+
+  playerChoice = (choice) => {
+    this.setState({
+      playerChoice: choice
+    }, () => {
+      console.log(this.state.playerChoice)
+    })
+  }
+
+  playerChoiceRock = () => {
+    this.playerChoice('rock');
+  }
+
+  playerChoiceScissors = () => {
+    this.playerChoice('scissors');
+  }
+
+  playerChoicePaper = () => {
+    this.playerChoice('paper');
   }
 
 
@@ -24,9 +45,13 @@ class App extends Component {
   return (
     <div className="App">
       <h1>Rock-Paper-Scissors</h1>
+      <h2>Your Choice: {this.state.playerChoice}</h2>
       <h2>player Score: {this.state.playerScore}</h2>
       <h2>computer Score: {this.state.compScore}</h2>
       <button onClick={this.getCompChoice}>Check</button>
+      <button onClick={this.playerChoiceRock}>Rock</button>
+      <button onClick={this.playerChoiceScissors}>Scissors</button>
+      <button onClick={this.playerChoicePaper}>Paper</button>
       
     </div>
   );
